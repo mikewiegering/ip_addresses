fb45b8e716d8af0fae8a14a5e417a16e985e83a6
ttafsir <ttafsir@gmail.com>
2020-04-24 18:09:52 +0000

    Add comments

--- suspicious.py
+++ suspicious.py
@@ -1,16 +1,17 @@
 import requests
 
 api_key = 'd5645a210a5065b0df6a2851db470f2a771ea67ce0975d39db1b3d31'
-# ip_address = '190.102.239.207'
-
-# api_url = f'https://api.ipdata.co/{ip_address}?api-key={api_key}'
-
-
 
 filename = 'addresses.txt'
+
+# open the file
 file_handle = open(filename, 'r')
+
+# read the contents of the file
 file_content = file_handle.read()
 
+# split the text of the file at each line, which
+# should correspond to an IP address.
 file_lines = file_content.split('\n')
 
 for ip in file_lines:
@@ -19,3 +20,6 @@ for ip in file_lines:
 
     if response.ok:
         print(response.json())
+
+# Make sure to close the file!
+file_handle.close()
